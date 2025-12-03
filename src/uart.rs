3d546ec7461449337fdd98797cc1ea13f117675e
Sebastian <me@wondering.website>
2025-12-03 13:36:14 -0500

    change to static global UART and led instances

--- src/uart.rs
+++ src/uart.rs
@@ -1,15 +1,19 @@
 use core::fmt::{self, Write};
-use core::sync::atomic::{AtomicBool, Ordering};
 use cortex_m::asm::nop;
 use mspm0l222x_pac::{Iomux, Uart0};
+use once_cell::sync::OnceCell;
 
 use crate::{PWREN_WRITE_KEY, RSTCTL_WRITE_KEY};
-
-static UART_SETUP: AtomicBool = AtomicBool::new(false);
 
 const UART_FREQUENCY: u32 = 115200;
 const TX_IOMUX: usize = 25 - 1;
 const RX_IOMUX: usize = 26 - 1;
+
+static UART: OnceCell<Uart> = OnceCell::new();
+
+pub fn uart() -> &'static Uart {
+    &UART.get().expect("uart not yet initialized")
+}
 
 // Taken from ti/driverlib/dl_uart.c
 const fn divisor(freq: u32) -> u32 {
@@ -38,7 +42,7 @@ impl Default for UartOptions {
         Self {
             hse: mspm0l222x_pac::uart0::uart0_ctl0::Hse::Ovs16,
             fen: Fen::Enable,
-            txe: Txe::Enable,
+            txe: Txe::Disable,
             rxe: Rxe::Enable,
             mode: Mode::Uart,
             ctsen: Ctsen::Disable,
@@ -51,88 +55,94 @@ impl Default for UartOptions {
     }
 }
 
-pub struct Uart<'a> {
-    regs: &'a Uart0,
+pub struct Uart {
+    regs: Uart0,
 }
-impl<'a> Uart<'a> {
-    pub fn new_with_config(opts: UartOptions, iomux: &Iomux, uart: &'a Uart0) -> Self {
-        if !UART_SETUP.load(Ordering::SeqCst) {
-            // set up IOMUX to output
-            iomux
-                .iomux_pincm(TX_IOMUX)
-                .write(|w| unsafe { w.pf().bits(2) });
+// TODO: is this fine?
+unsafe impl Send for Uart {}
+unsafe impl Sync for Uart {}
 
-            iomux
-                .iomux_pincm(RX_IOMUX)
-                .write(|w| unsafe { w.pf().bits(2) });
+impl Uart {
+    pub fn init(iomux: &Iomux, uart: Uart0) {
+        let _ = UART.get_or_init(|| Uart::new(iomux, uart));
+    }
 
-            // Disable UART before configuration
-            uart.uart0_gprcm(0).uart0_rstctl().write(|w| {
-                unsafe { w.bits(RSTCTL_WRITE_KEY) }
-                    .resetassert()
-                    .assert()
-                    .resetstkyclr()
-                    .clr()
-            });
+    fn new_with_config(opts: UartOptions, iomux: &Iomux, uart: Uart0) -> Self {
+        // set up IOMUX to output
+        iomux
+            .iomux_pincm(TX_IOMUX)
+            .write(|w| unsafe { w.pf().bits(2) });
 
-            uart.uart0_gprcm(0)
-                .uart0_pwren()
-                .write(|w| unsafe { w.bits(PWREN_WRITE_KEY) }.enable().set_bit());
-            // TODO: wait several cycles? does this need to be changed?
-            for _ in core::hint::black_box(0..32) {}
+        iomux
+            .iomux_pincm(RX_IOMUX)
+            .write(|w| unsafe { w.pf().bits(2) });
 
-            // Select clock source (BUSCLK) and divisor
-            uart.uart0_clksel().write(|w| w.busclk_sel().enable());
-            uart.uart0_clkdiv().write(|w| w.ratio().div_by_1());
-            // disable UART
-            uart.uart0_ctl0().write(|w| w.enable().clear_bit());
+        // Disable UART before configuration
+        uart.uart0_gprcm(0).uart0_rstctl().write(|w| {
+            unsafe { w.bits(RSTCTL_WRITE_KEY) }
+                .resetassert()
+                .assert()
+                .resetstkyclr()
+                .clr()
+        });
 
-            // Set baud rate divisors
-            let div = divisor(opts.uart_freq);
-            uart.uart0_ibrd()
-                .write(|w| unsafe { w.divint().bits((div >> 6) as u16) });
-            uart.uart0_fbrd()
-                .write(|w| unsafe { w.divfrac().bits((div & 0b111111) as u8) });
+        uart.uart0_gprcm(0)
+            .uart0_pwren()
+            .write(|w| unsafe { w.bits(PWREN_WRITE_KEY) }.enable().set_bit());
+        // TODO: wait several cycles? does this need to be changed?
+        for _ in core::hint::black_box(0..32) {}
 
-            // set all UART settings
-            uart.uart0_ctl0().write(|w| {
-                w.hse()
-                    .variant(opts.hse)
-                    .fen()
-                    .variant(opts.fen)
-                    .txe()
-                    .variant(opts.txe)
-                    .rxe()
-                    .variant(opts.rxe)
-                    .mode()
-                    .variant(opts.mode)
-                    .ctsen()
-                    .variant(opts.ctsen)
-                    .rtsen()
-                    .variant(opts.rtsen)
-            });
+        // disable UART
+        uart.uart0_ctl0().write(|w| w.enable().clear_bit());
 
-            // Configure line control
-            uart.uart0_lcrh().write(|w| {
-                w.pen()
-                    .variant(opts.pen)
-                    .wlen()
-                    .variant(opts.wlen)
-                    .stp2()
-                    .variant(opts.stp2)
-            });
+        // Select clock source (BUSCLK) and divisor
+        uart.uart0_clksel().write(|w| w.busclk_sel().enable());
+        uart.uart0_clkdiv().write(|w| w.ratio().div_by_1());
 
-            // enable UART
-            // if you don't set fen here, it resets to 0. why? who knows
-            uart.uart0_ctl0()
-                .write(|w| w.enable().enable().fen().variant(opts.fen));
+        // Set baud rate divisors
+        let div = divisor(opts.uart_freq);
+        uart.uart0_ibrd()
+            .write(|w| unsafe { w.divint().bits((div >> 6) as u16) });
+        uart.uart0_fbrd()
+            .write(|w| unsafe { w.divfrac().bits((div & 0b111111) as u8) });
 
-            UART_SETUP.store(true, Ordering::Relaxed);
-        }
+        // set all UART settings
+        uart.uart0_ctl0().write(|w| {
+            w.hse()
+                .variant(opts.hse)
+                .fen()
+                .variant(opts.fen)
+                .txe()
+                .variant(opts.txe)
+                .rxe()
+                .variant(opts.rxe)
+                .mode()
+                .variant(opts.mode)
+                .ctsen()
+                .variant(opts.ctsen)
+                .rtsen()
+                .variant(opts.rtsen)
+                .txd_out_en()
+                .enable()
+        });
+
+        // Configure line control
+        uart.uart0_lcrh().write(|w| {
+            w.pen()
+                .variant(opts.pen)
+                .wlen()
+                .variant(opts.wlen)
+                .stp2()
+                .variant(opts.stp2)
+        });
+
+        // enable UART
+        uart.uart0_ctl0().modify(|_, w| w.enable().enable());
+
         Self { regs: uart }
     }
 
-    pub fn new(iomux: &Iomux, uart: &'a Uart0) -> Self {
+    fn new(iomux: &Iomux, uart: Uart0) -> Self {
         Uart::new_with_config(UartOptions::default(), iomux, uart)
     }
 
@@ -172,9 +182,13 @@ impl<'a> Uart<'a> {
     pub fn busy(&self) -> bool {
         self.regs.uart0_stat().read().busy().bit_is_set()
     }
+
+    pub fn set_tx(&self, state: bool) {
+        self.regs.uart0_ctl0().modify(|_, w| w.txd_out().bit(state));
+    }
 }
 
-impl<'a> Write for Uart<'a> {
+impl<'a> Write for Uart {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_bytes(s.as_bytes());
 
