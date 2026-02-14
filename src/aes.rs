5fa7fcefb28cc9be618c92cbb66daada6868c994
Sebastian <me@wondering.website>
2026-02-13 19:55:46 -0500

    use safe registers for AES

--- src/aes.rs
+++ src/aes.rs
@@ -1,25 +1,8 @@
-use once_cell::sync::OnceCell;
 use cortex_m::asm::nop;
+use mspm0l222x_pac::Aesadv as PacAesadv;
+use once_cell::sync::OnceCell;
 
-pub const AES_BASE_ADDR: usize = 0x40442000;
-
-// offsets
-const REG_PWREN:        isize = 0x800;
-const REG_RSTCTL:       isize = 0x804;
-const REG_AES_KEY0:     isize = 0x1120; 
-const REG_AES_CTRL:     isize = 0x1150; 
-const REG_AES_C_LEN0:   isize = 0x1154; 
-const REG_AES_DATA0:    isize = 0x1160; 
-
-const PWREN_KEY: u32 = 0x26;
-const RSTCTL_KEY: u32 = 0xB1;
-
-// CTRL bitmasks
-const CTRL_CNTXT_RDY:  u32 = 1 << 31;
-const CTRL_INPUT_RDY:  u32 = 1 << 1;
-const CTRL_OUTPUT_RDY: u32 = 1 << 0;
-const CTRL_DIR_ENCRYPT: u32 = 1 << 2;
-const CTRL_KEYSIZE_128: u32 = 1 << 3; 
+use crate::{PWREN_WRITE_KEY, RSTCTL_WRITE_KEY};
 
 static AES: OnceCell<AesAdv> = OnceCell::new();
 
@@ -28,86 +11,85 @@ pub fn aes() -> &'static AesAdv {
 }
 
 pub struct AesAdv {
-    base_addr: usize,
+    aes: PacAesadv,
 }
 
 unsafe impl Send for AesAdv {}
 unsafe impl Sync for AesAdv {}
 
 impl AesAdv {
-    fn new(base_addr: usize) -> Self {
-        AesAdv { base_addr }
-    }
-
-    pub fn init() {
-        let _ = AES.get_or_init(|| {
-            let instance = AesAdv::new(AES_BASE_ADDR);
-            instance.hw_init();
-            instance
+    fn new(aes: PacAesadv) -> Self {
+        aes.aesadv_gprcm(0).aesadv_rstctl().write(|w| {
+            unsafe { w.bits(RSTCTL_WRITE_KEY) }
+                .resetassert()
+                .assert()
+                .resetstkyclr()
+                .clr()
         });
+
+        aes.aesadv_gprcm(0)
+            .aesadv_pwren()
+            .write(|w| unsafe { w.bits(PWREN_WRITE_KEY) }.enable().set_bit());
+
+        AesAdv { aes }
     }
 
-    unsafe fn write_reg(&self, offset: isize, value: u32) {
-        let addr = (self.base_addr as *mut u32).offset(offset / 4);
-        addr.write_volatile(value);
+    pub fn init(aes: PacAesadv) {
+        let _ = AES.get_or_init(|| AesAdv::new(aes));
     }
 
-    unsafe fn read_reg(&self, offset: isize) -> u32 {
-        let addr = (self.base_addr as *mut u32).offset(offset / 4);
-        addr.read_volatile()
+    pub fn set_key(&self, key: &[u8; 32]) {
+        while self.aes.aesadv_ctrl().read().cntxt_rdy().is_notready() {}
+
+        let key: &[u32; 4] = bytemuck::cast_ref(key);
+
+        self.aes.aesadv_key0().write(|w| unsafe { w.bits(key[0]) });
+        self.aes.aesadv_key1().write(|w| unsafe { w.bits(key[1]) });
+        self.aes.aesadv_key2().write(|w| unsafe { w.bits(key[2]) });
+        self.aes.aesadv_key3().write(|w| unsafe { w.bits(key[3]) });
     }
 
-    fn hw_init(&self) {
-        unsafe {
-            // power and reset sequence
-            self.write_reg(REG_RSTCTL, (RSTCTL_KEY << 24) | 1);
-            for _ in 0..100 { nop(); }
-            self.write_reg(REG_RSTCTL, (RSTCTL_KEY << 24) | 0);
-            for _ in 0..1000 { nop(); }
-            self.write_reg(REG_PWREN, (PWREN_KEY << 24) | 1);
-            for _ in 0..5000 { nop(); }
-        }
-    }
-
-    pub fn set_key(&self, key: &[u32; 4]) -> bool {
-        unsafe {
-            while (self.read_reg(REG_AES_CTRL) & CTRL_CNTXT_RDY) == 0 { nop(); }
-            
-            for (i, &k) in key.iter().enumerate() {
-                self.write_reg(REG_AES_KEY0 + (i as isize * 4), k.swap_bytes());
-            }
-        }
-        true
-    }
-
-    pub fn encrypt_block(&self, plaintext: &[u32; 4]) -> [u32; 4] {
+    pub fn encrypt_block(&self, plaintext: &[u8; 32]) -> [u8; 32] {
         self.process_block(plaintext, true)
     }
 
-    fn process_block(&self, input_data: &[u32; 4], is_encrypt: bool) -> [u32; 4] {
-        let mut output_data = [0u32; 4];
-        
-        unsafe {
-            let mut ctrl = CTRL_KEYSIZE_128;
-            if is_encrypt {
-                ctrl |= CTRL_DIR_ENCRYPT;
-            }
-            self.write_reg(REG_AES_CTRL, ctrl);
+    fn process_block(&self, input_data: &[u8; 32], is_encrypt: bool) -> [u8; 32] {
+        let mut output_data = [0u8; 32];
+        self.aes
+            .aesadv_ctrl()
+            .write(|w| w.keysize().k128().dir().bit(is_encrypt));
+        self.aes
+            .aesadv_c_length_0()
+            .write(|w| unsafe { w.bits(16) });
 
-            self.write_reg(REG_AES_C_LEN0, 16); 
-
-            while (self.read_reg(REG_AES_CTRL) & CTRL_INPUT_RDY) == 0 { nop(); }
-
-            for (i, &val) in input_data.iter().enumerate() {
-                self.write_reg(REG_AES_DATA0 + (i as isize * 4), val.swap_bytes());
-            }
-
-            while (self.read_reg(REG_AES_CTRL) & CTRL_OUTPUT_RDY) == 0 { nop(); }
-
-            for (i, val) in output_data.iter_mut().enumerate() {
-                *val = self.read_reg(REG_AES_DATA0 + (i as isize * 4)).swap_bytes();
-            }
+        while self.aes.aesadv_ctrl().read().cntxt_rdy().is_notready() {
+            nop();
         }
+
+        let input_data: &[u32; 4] = bytemuck::cast_ref(input_data);
+
+        self.aes
+            .aesadv_data0()
+            .write(|w| unsafe { w.bits(input_data[0]) });
+        self.aes
+            .aesadv_data1()
+            .write(|w| unsafe { w.bits(input_data[1]) });
+        self.aes
+            .aesadv_data2()
+            .write(|w| unsafe { w.bits(input_data[2]) });
+        self.aes
+            .aesadv_data3()
+            .write(|w| unsafe { w.bits(input_data[3]) });
+
+        while self.aes.aesadv_ctrl().read().output_rdy().is_notready() {
+            nop();
+        }
+
+        let output_data_view: &mut [u32; 4] = bytemuck::cast_mut(&mut output_data);
+        output_data_view[0] = self.aes.aesadv_data0().read().bits();
+        output_data_view[1] = self.aes.aesadv_data1().read().bits();
+        output_data_view[2] = self.aes.aesadv_data2().read().bits();
+        output_data_view[3] = self.aes.aesadv_data3().read().bits();
 
         output_data
     }
