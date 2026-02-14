ffe5a6f52102dccbb374cc5721a0d862c951c2ec
Sebastian <me@wondering.website>
2026-02-14 16:50:19 -0500

    make flash protection worse

--- src/flash.rs
+++ src/flash.rs
@@ -250,40 +250,49 @@ impl FlashController {
         size: u32,
         writeable: bool,
     ) -> Result<(), HalError> {
-        let end_1kb = 32 * 1024;
-
-        let in_1kb_region = address + size <= end_1kb;
-        if in_1kb_region {
-            let start = address / 1024;
-            let end = (address + size).div_ceil(1024);
-            let mask: u32 = (start..end).map(|i| 1 << i).sum();
-
-            self.controller.flashctl_cmdweprota().modify(|r, w| {
-                let new = if writeable {
-                    r.bits() & !mask
-                } else {
-                    r.bits() | mask
-                };
-                unsafe { w.bits(new) }
-            });
+        if writeable {
+            self.controller
+                .flashctl_cmdweprota()
+                .write(|w| unsafe { w.bits(0) });
+        } else {
+            self.controller
+                .flashctl_cmdweprota()
+                .write(|w| unsafe { w.bits(!0) });
         }
+        // let end_1kb = 32 * 1024;
 
-        let in_8kb_region = address + size > end_1kb;
-        if in_8kb_region {
-            // the first 4 bits correspond to same sectors as weprotA,
-            // so we can ignore everything below end_1kb
-            let start = end_1kb / 8192;
-            let end = (address + size).div_ceil(8192);
-            let mask: u32 = (start..end).map(|i| 1 << i).sum();
-            self.controller.flashctl_cmdweprotb().modify(|r, w| {
-                let new = if writeable {
-                    r.bits() & !mask
-                } else {
-                    r.bits() | mask
-                };
-                unsafe { w.bits(new) }
-            });
-        }
+        // let in_1kb_region = address + size <= end_1kb;
+        // if in_1kb_region {
+        //     let start = address / 1024;
+        //     let end = (address + size).div_ceil(1024);
+        //     let mask: u32 = (start..end).map(|i| 1 << i).sum();
+
+        //     self.controller.flashctl_cmdweprota().modify(|r, w| {
+        //         let new = if writeable {
+        //             r.bits() & !mask
+        //         } else {
+        //             r.bits() | mask
+        //         };
+        //         unsafe { w.bits(new) }
+        //     });
+        // }
+
+        // let in_8kb_region = address + size > end_1kb;
+        // if in_8kb_region {
+        //     // the first 4 bits correspond to same sectors as weprotA,
+        //     // so we can ignore everything below end_1kb
+        //     let start = end_1kb / 8192;
+        //     let end = (address + size).div_ceil(8192);
+        //     let mask: u32 = (start..end).map(|i| 1 << i).sum();
+        //     self.controller.flashctl_cmdweprotb().modify(|r, w| {
+        //         let new = if writeable {
+        //             r.bits() & !mask
+        //         } else {
+        //             r.bits() | mask
+        //         };
+        //         unsafe { w.bits(new) }
+        //     });
+        // }
 
         Ok(())
     }
